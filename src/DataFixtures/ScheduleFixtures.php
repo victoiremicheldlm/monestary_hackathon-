6ded484ed7083676533364d8da6500a8b00dc7a9
FlorianBetin <florian.betin.pro@gmail.com>
2023-01-12 11:59:22 +0100

    schedule and commentvehicule

---
+++ src/DataFixtures/ScheduleFixtures.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\DataFixtures;
+
+use DateTime;
+use Faker\Factory;
+use DateTimeImmutable;
+use App\Entity\Schedule;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+
+class ScheduleFixtures extends Fixture implements DependentFixtureInterface
+{
+    public static int $scheduleIndex = 0;
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create();
+        for ($j = 1; $j <=10; $j++) {
+            self::$scheduleIndex++;
+                $schedule = new Schedule();
+                $schedule->setVehicle($this->getReference('vehicle_' . rand(1, VehicleFixtures::$vehicleIndex)));
+                $schedule->setStartAt($faker->dateTimeBetween('-2 weeks' , '-1 week'));
+                $schedule->setEndAt($faker->dateTimeBetween('-1 week' , '+1 week'));
+                $manager->persist($schedule);
+                $this->addReference('schedule_' . self::$scheduleIndex, $schedule);
+                
+        }
+            $manager->flush();
+    }
+
+    
+    public function getDependencies(): array
+    {
+        return [
+           VehicleFixtures::class,
+           UserFixtures::class,
+        ];
+    }
+}
